37ecf2baa7ea81a693894709f8c1af8c2c6f86ed
sandeep2720 <245122748030@mvsrec.edu.in>
2025-03-24 13:06:02 +0530

    Add files via upload

---
+++ cmdl.py
@@ -0,0 +1,12 @@
+import sys
+n1 = int(sys.argv[1])
+n2 = int(sys.argv[2])
+r1 = n1+n2
+r2 = n1-n2
+r3 = n1*n2
+r4 = n1/n2
+
+print("Addition:",r1)
+print("Addition:",r2)
+print("Addition:",r3)
+print("Addition:",r4)
