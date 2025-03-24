6f99842a712cbbd333f7d47d6773019f3c25da9f
sandeep2720 <245122748030@mvsrec.edu.in>
2025-03-24 12:56:24 +0530

    Add files via upload

---
+++ Cmdl2.java
@@ -0,0 +1,13 @@
+class Cmdl2
+{
+public static void main(String args[])
+{
+String s1 = args[0];
+String s2 = args[1];
+String s3 = args[2];
+
+System.out.println("Name:"+s1);
+System.out.println("Name:"+s2);
+System.out.println("Name:"+s3);
+}
+}
